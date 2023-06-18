9cf9bd799e259e4bf95accf6ee1cdd3e202d10e3
SumiAkter2 <akteralisha36@gmail.com>
2023-06-18 10:30:17 +0600

    category product done

--- src/Component/Category/CategoryProduct.jsx
+++ src/Component/Category/CategoryProduct.jsx
@@ -1,14 +1,22 @@
 
 import UseHook from "../../Hook/UseHook";
 import CategoryProductInfo from "./CategoryProductInfo";
-
+import {IoMdArrowDroprightCircle} from 'react-icons/io'
+import {IoMdArrowDropleftCircle} from 'react-icons/io'
 
 const CategoryProduct = () => {
     const [products] = UseHook();
     return (
-        <div className="text-left">
-            <h1 className="tittle">Shop by Category</h1>
-            <p className="sub-tittle text-left">Life is hard enough already. Let us make it a little easier.</p>
+        <div className="">
+            <h1 className="tittle text-left">Shop by Category</h1>
+           
+            <div className=" flex justify-between transform -translate-y-1/2 ">
+                 <p className="sub-tittle text-left">Life is hard enough already. Let us make it a little easier.</p>
+                <div className="flex cursor-pointer">
+                    <IoMdArrowDroprightCircle size='25px' />
+                    <IoMdArrowDropleftCircle size='25px'/>
+       </div>
+      </div>
             <div className="grid lg:grid-cols-3 mx-auto">
                 {
                     products.slice(7,16).map(product=><CategoryProductInfo key={product.id} product={product}></CategoryProductInfo>)
